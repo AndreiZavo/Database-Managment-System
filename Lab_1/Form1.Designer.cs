1e1ebf5ca2fdf7024f7d427fc15e243485320988
AndreiZavo <andrei_lucian01@yahoo.com>
2021-03-19 08:39:25 +0200

    Lab 1

---
+++ Lab_1/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Lab_2_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvClients = new System.Windows.Forms.DataGridView();
+            this.dgvOrders = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveDataButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClients)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvClients
+            // 
+            this.dgvClients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClients.Location = new System.Drawing.Point(77, 118);
+            this.dgvClients.Name = "dgvClients";
+            this.dgvClients.RowHeadersWidth = 102;
+            this.dgvClients.RowTemplate.Height = 40;
+            this.dgvClients.Size = new System.Drawing.Size(1340, 331);
+            this.dgvClients.TabIndex = 0;
+            // 
+            // dgvOrders
+            // 
+            this.dgvOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrders.Location = new System.Drawing.Point(77, 569);
+            this.dgvOrders.Name = "dgvOrders";
+            this.dgvOrders.RowHeadersWidth = 102;
+            this.dgvOrders.RowTemplate.Height = 40;
+            this.dgvOrders.Size = new System.Drawing.Size(1340, 367);
+            this.dgvOrders.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Clients";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 509);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Orders";
+            // 
+            // saveDataButton
+            // 
+            this.saveDataButton.Location = new System.Drawing.Point(1845, 460);
+            this.saveDataButton.Name = "saveDataButton";
+            this.saveDataButton.Size = new System.Drawing.Size(182, 129);
+            this.saveDataButton.TabIndex = 4;
+            this.saveDataButton.Text = "Save Data";
+            this.saveDataButton.UseVisualStyleBackColor = true;
+            this.saveDataButton.Click += new System.EventHandler(this.saveDataButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2180, 985);
+            this.Controls.Add(this.saveDataButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvOrders);
+            this.Controls.Add(this.dgvClients);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClients)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvClients;
+        private System.Windows.Forms.DataGridView dgvOrders;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button saveDataButton;
+    }
+}
+
---
+++ Lab_1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab_2_Example
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        DataSet ds;
+        SqlDataAdapter daClients, daOrders;
+        SqlCommandBuilder cbOrders;
+        BindingSource bsClients, bsOrders;
+
+        private void saveDataButton_Click(object sender, EventArgs e)
+        {
+            daOrders.Update(ds, "Orders");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection("Data Source=DESKTOP-OFELIA;Initial Catalog=Sem2_Tennis_Table;Integrated Security=True");
+            ds = new DataSet();
+            daClients = new SqlDataAdapter("SELECT * FROM Clients", conn);
+            daOrders = new SqlDataAdapter("SELECT * FROM Orders", conn);
+            cbOrders = new SqlCommandBuilder(daOrders);
+
+            daClients.Fill(ds, "Clients");
+            daOrders.Fill(ds, "Orders");
+
+            Console.WriteLine(ds.Tables["Clients"].Constraints.Count);
+            Console.WriteLine(ds.Tables["Orders"].Constraints.Count);
+
+            DataRelation dr = new DataRelation("FK_Orders_Clients", ds.Tables["Clients"].Columns["CID"],
+                ds.Tables["Orders"].Columns["CID"]);
+
+            ds.Relations.Add(dr);
+
+            Console.WriteLine(ds.Tables["Clients"].Constraints[0].GetType());
+            Console.WriteLine(ds.Tables["Orders"].Constraints[0].GetType());
+
+            // UniqueConstraint, ForeignKeyConstraint
+            // GetChildRows, GetParentRow
+
+            bsClients = new BindingSource();
+            bsClients.DataSource = ds;
+            bsClients.DataMember = "Clients";
+            
+            bsOrders = new BindingSource();
+            bsOrders.DataSource = bsClients;
+            bsOrders.DataMember = "FK_Orders_Clients";
+
+            dgvClients.DataSource = bsClients;
+            dgvOrders.DataSource = bsOrders;
+
+        }
+    }
+}
